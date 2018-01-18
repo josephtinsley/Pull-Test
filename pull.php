8b15ef91a1bd5b074268befdc65facc69933ed19
Joe <joseph.tinsley@mgail.com>
2018-01-18 09:01:18 -0500

    New Code block

--- pull.php
+++ pull.php
@@ -31,6 +31,12 @@
 		    <h4>Enter in some random text!</h4>
                 </div>
             </div>  
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12  col-xs-12">
+                    <h1>Pull 3!</h1>
+                    <?PHP echo 'Current time '. date("Y-m-d H:i:s",time());?>
+                </div>
+            </div>
         </div>    
     </body>
 </html>
