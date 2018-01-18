c2c887c088d2ae49efa9e7a3be170bf5e6e878a3
Joe <joseph.tinsley@mgail.com>
2018-01-18 09:15:24 -0500

    Adding a background color

--- pull.php
+++ pull.php
@@ -15,6 +15,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
+        <style>
+            body{
+                background-color: #f2f2f2;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
